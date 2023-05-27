b01d46268081029cfcb89d9d7235dd467e540383
Judith <judith.ovando.l@gmail.com>
2023-05-27 03:20:52 -0400

    diseño casi ok, bd ok, pendiente lógica

---
+++ src/Components/List.jsx
@@ -0,0 +1,26 @@
+const List = ({collabs,search}) => {
+
+    return (
+
+        <table class="table">
+          <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            </tr>
+          </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            </tr>
+    
+        </tbody>
+       </table>
+    )
+}
+
+export default List;
